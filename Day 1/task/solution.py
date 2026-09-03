56f9c4aca6e216570162535d3d75efcc1b0171c1
Sami <togetsami116@gmail.com>
2026-09-03 10:42:17 +0300

    initial comit

---
+++ Day 1/task/solution.py
@@ -0,0 +1,6 @@
+# Write your code below this line 👇
+print("Welcome to the band name Generator")
+Country_Name = input("What is The name of the city you grew up?\n")
+User_Name = input("What is your Name\n")
+Band_Name = print( "Your Band Name is"+ " " + Country_Name + " " + User_Name)
+# Write your code below this line 👇
